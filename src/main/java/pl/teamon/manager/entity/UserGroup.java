f70e0a8daefaf4b4de0c8494efeb774ad472b1af
AdrianSzromba <adrian.szromba@gmail.com>
2018-01-09 11:21:21 +0100

    add new entity UserGroup

---
+++ src/main/java/pl/teamon/manager/entity/UserGroup.java
@@ -0,0 +1,23 @@
+package pl.teamon.manager.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "user_group")
+public class UserGroup {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
+	private long id;
+
+	@Column(unique = true)
+	@NotNull
+	private String groupName;
+}
