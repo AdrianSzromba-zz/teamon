b19044f306b286ad1c5bbc84b181fb75130a2a86
AdrianSzromba <adrian.szromba@gmail.com>
2018-01-09 20:28:25 +0100

    UserGroup add

--- src/main/java/pl/teamon/manager/entity/UserGroup.java
+++ src/main/java/pl/teamon/manager/entity/UserGroup.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -20,4 +22,8 @@ public class UserGroup {
 	@Column(unique = true)
 	@NotNull
 	private String groupName;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 }
