f7a54f8d85c001fef95caea413e471d97ebd712f
AdrianSzromba <adrian.szromba@gmail.com>
2018-01-11 17:26:50 +0100

    add new table for group and user

--- src/main/java/pl/teamon/manager/entity/User.java
+++ src/main/java/pl/teamon/manager/entity/User.java
@@ -1,12 +1,19 @@
 package pl.teamon.manager.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -22,9 +29,11 @@ public class User {
 	private long id;
 
 	@NotEmpty
+	@Size(min = 2, max = 15)
 	private String username;
 	
 	@NotEmpty
+	@Size(min = 2, max = 30)
 	private String userlastname;
 
 	@NotEmpty
@@ -37,6 +46,13 @@ public class User {
 	@Email
 	@Column(unique = true)
 	private String email;
+	
+	@ManyToMany (cascade =CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
+	private List<UserGroup> userGroup = new ArrayList<>();
+	
+	public String getFullName() {
+		return this.username + "   " + this.userlastname+ "   " + this.email;
+	}
 
 	public long getId() {
 		return id;
@@ -89,4 +105,13 @@ public class User {
 	public void setUserlastname(String userlastname) {
 		this.userlastname = userlastname;
 	}
+
+	public List<UserGroup> getUserGroup() {
+		return userGroup;
+	}
+
+	public void setUserGroup(List<UserGroup> userGroup) {
+		this.userGroup = userGroup;
+	}
+
 }
--- src/main/java/pl/teamon/manager/entity/UserGroup.java
+++ src/main/java/pl/teamon/manager/entity/UserGroup.java
@@ -1,12 +1,16 @@
 package pl.teamon.manager.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -22,8 +26,32 @@ public class UserGroup {
 	@Column(unique = true)
 	@NotNull
 	private String groupName;
-	
-	@ManyToOne
-	@JoinColumn(name = "user_id")
-	private User user;
+
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private List<User> user = new ArrayList<>();
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	public List<User> getUser() {
+		return user;
+	}
+
+	public void setUser(List<User> user) {
+		this.user = user;
+	}
+
 }
